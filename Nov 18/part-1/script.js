25476570c9a1d4198509a42311ec1b8b22de68af
Mohd Saif <121762403+mohdsaifprototype@users.noreply.github.com>
2024-11-21 13:17:12 +0530

    November 18

---
+++ Nov 18/part-1/script.js
@@ -0,0 +1,11 @@
+/* 
+Challange:
+Guess the no.
+If the guess is smaller print guess a larger no.
+If the guess is greater print guess a smaller no.
+If the guess is correct print you won.
+  also print "How many attempts you took to guess the no."
+
+Hint: USE BUILT-IN METHODS FROM MATH CLASS FROM JS
+*/
+
