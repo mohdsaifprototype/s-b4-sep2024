1694b5d5a36ccd314894e27c17b178bd21a1fd8e
Mohd Saif <121762403+mohdsaifprototype@users.noreply.github.com>
2024-12-30 17:20:45 +0530

    December 30
    Date and Time part-1

---
+++ Dec 30/part-1/script.js
@@ -0,0 +1,14 @@
+/* 
+Topic: 
+*/
+
+/* Date & time, setTimeOut(), setInterval */
+
+let now = new Date();
+
+let calculate = now.getTime() / (1000 * 60 * 60 * 24 * 365.25);
+
+console.log(calculate);
+
+
+console.log("Now", now.getHours());
