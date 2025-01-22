e6cced551ca5689bce99fb02624dfaede1998c83
Mohd Saif <121762403+mohdsaifprototype@users.noreply.github.com>
2025-01-22 16:03:24 +0530

    January 22

---
+++ Jan 22/script.js
@@ -0,0 +1,28 @@
+// const myBox = document.querySelector(".box");
+// let randomValue = 0;
+/* myBox.addEventListener("mouseover", (e) => {
+  randomValue = Math.round(Math.random() * 100);
+  console.log("drag started");
+  console.log("event is ", e);
+  console.log("Random value", randomValue);
+
+  myBox.style.left = e.clientX + randomValue + "px";
+  myBox.style.top = e.clientY + randomValue + "px";
+}); */
+
+// myBox.addEventListener("drag", (e) => {
+//   randomValue = Math.round(Math.random() * 100);
+//   console.log("drag started");
+//   console.log("event is ", e);
+//   console.log("Random value", randomValue);
+
+//   myBox.style.left = e.movementX + randomValue + "px";
+//   myBox.style.top = e.movementY + randomValue + "px";
+// });
+
+
+/* document.body.addEventListener("click",(event) => {
+  console.log("Event triggered at", event.target);
+  console.log("Event triggered at", event.target.innerHTML);
+  event.target.style.display = "none";
+}) */
